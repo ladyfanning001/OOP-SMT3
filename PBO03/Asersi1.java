9a1950af308483e6d27540b454608fc9b0afd0d3
Ladya Kalascha <155611207+ladyfanning001@users.noreply.github.com>
2025-03-09 20:39:54 +0700

    Create Asersi1.java

---
+++ PBO03/Asersi1.java
@@ -0,0 +1,20 @@
+/** 
+ * File : Asersi1.java
+ * Deskripsi : Program untuk menunjukkan Asersi
+ * Nama/NIM : Ladya Kalascha/24060123140133
+ * 
+*/
+
+package PBO03;
+
+public class Asersi1{
+    public static void main(String[] args) {
+        int x = 0;
+        if (x > 0){
+            System.out.println("x bilangan positif");
+        }
+        else{
+            System.out.println("x bilangan negatif");
+        }
+    }
+}
