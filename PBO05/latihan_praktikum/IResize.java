6be779f0286b4abd1f58febf2b78fb7225ca4b34
Ladya K <155611207+ladyfanning001@users.noreply.github.com>
2025-03-24 20:30:50 +0700

    Add files via upload

---
+++ PBO05/latihan_praktikum/IResize.java
@@ -0,0 +1,13 @@
+/* 
+  Nmaa/NIM   : Ladya Kalascha/24060123140133
+  Tanggal   : 21 Maret 2025
+*/
+
+public interface IResize {
+    void zoomIn();
+  
+    void zoomOut();
+  
+    void zoom(double percent);
+  }
+  
