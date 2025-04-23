816db13ccbb2a1f6d966f292ce70e66c58ca1081
Ladya K <155611207+ladyfanning001@users.noreply.github.com>
2025-04-23 11:50:44 +0700

    Add files via upload

---
+++ PBO07/post test/TestPolimorfisme.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TestPolimorfisme {
+    public static void main(String[] args) {
+        Pegawai pegawai = new Programmer("Mira");
+        Pegawai pegawai2 = new Manajer("Joko");
+        Manajer pegawai3 = new Manajer("Argo");
+
+        ArrayList<Pegawai> emps = new ArrayList<>();
+        emps.add(pegawai);
+        emps.add(pegawai2);
+        emps.add(pegawai3);
+
+        for (Pegawai emp : emps) {
+            emp.tampilData();
+        }
+    }
+}
