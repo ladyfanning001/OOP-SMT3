3c08b81b1f432503c7e91e3076b9fe90038c775d
Ladya K <155611207+ladyfanning001@users.noreply.github.com>
2025-05-03 21:20:43 +0700

    Add files via upload

---
+++ PBO08-GENERICS/10e/KontrolSenjata.java
@@ -0,0 +1,51 @@
+package com.mycompany.latihanmedium;
+
+
+public class KontrolSenjata {
+    private Senjata senjata;
+
+    public KontrolSenjata(Senjata s) {
+        this.senjata = s;
+    }
+
+    public boolean isAdaPeluru() {
+        return senjata.getPeluru() > 0;
+    }
+
+    public void isiPeluru(int jumPeluru) {
+        int total = senjata.getPeluru() + jumPeluru;
+        senjata.setPeluru(total);
+        System.out.println(">> Peluru berhasil ditambah: " + jumPeluru);
+    }
+
+    public void menembak(int jumlah) {
+        System.out.println(">> Siap menembak " + jumlah + " kali");
+        if (!isAdaPeluru()) {
+            System.out.println("Peluru Habis");
+            return;
+        }
+
+        for (int i = 0; i < jumlah; i++) {
+            if (senjata.getPeluru() > 0) {
+                System.out.println(senjata.getBunyi());
+                senjata.setPeluru(senjata.getPeluru() - 1);
+            } else {
+                System.out.println("Gagal tembak, Peluru Habis");
+            }
+        }
+
+        System.out.println(">> Peluru sisa: " + senjata.getPeluru());
+    }
+
+    public String menusuk() {
+        if (senjata.isMenusuk()) {
+            return "Jleb!";
+        } else {
+            return "Senjata tidak memiliki bayonet.";
+        }
+    }
+
+    public void pasangBayonet() {
+        senjata.setMenusuk(true);
+    }
+}
