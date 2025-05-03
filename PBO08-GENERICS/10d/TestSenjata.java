ff6d0ce384197fbed7051c397986cb15be419d61
Ladya K <155611207+ladyfanning001@users.noreply.github.com>
2025-05-03 21:23:59 +0700

    Update and rename latihanTutorial.java to TestSenjata.java

--- PBO08-GENERICS/10d/TestSenjata.java
+++ PBO08-GENERICS/10d/TestSenjata.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package latihanTutorial;
-
-/**
- *
- * @author ladyakalascha
- */
 
 public class latihanTutorial
 {
